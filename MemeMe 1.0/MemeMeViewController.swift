84db2c4bcea6b5a5d5b1fc7d7b6905bad31db524
Bill <brubaker.bill@gmail.com>
2016-08-21 14:56:46 -0500

    Initial Commit
    Currently, UIToolbar and UITextFields are behaving as desired.
    
    UIImageView needs work, specifically: it needs to fills the
    UIImageView, but not go out of bounds.

---
+++ MemeMe 1.0/MemeMeViewController.swift
@@ -0,0 +1,101 @@
+//
+//  MemeMeViewController.swift
+//  MemeMe 1.0
+//
+//  Created by William Brubaker on 8/19/16.
+//  Copyright © 2016 William Brubaker. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemeMeViewController: UIViewController, UIImagePickerControllerDelegate, UITextFieldDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imagePickedView: UIImageView!
+    @IBOutlet weak var cameraPickerButton: UIBarButtonItem!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+    //instantiate delegates, if necessary
+    
+    enum SourceSelection: Int {
+        case Album = 0, Camera
+    }
+    
+    let memeTextAttributes = [
+        NSStrokeColorAttributeName : UIColor.blackColor(),
+        NSForegroundColorAttributeName : UIColor.whiteColor(),
+        NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSStrokeWidthAttributeName : 3.0
+    ]
+    
+    override func viewWillAppear(animated: Bool) {
+        cameraPickerButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
+    }
+    
+    override func viewDidLoad() {
+        topTextField.delegate = self
+        topTextField.text = "TOP"
+        topTextField.textAlignment = NSTextAlignment.Center
+        topTextField.defaultTextAttributes = memeTextAttributes
+        
+        bottomTextField.delegate = self
+        bottomTextField.text = "BOTTOM"
+        bottomTextField.textAlignment = NSTextAlignment.Center
+        bottomTextField.defaultTextAttributes = memeTextAttributes
+        
+        imagePickedView.image = UIImage(named: "PixelArt")
+        //imagePickedView.image = UIImage(named: "Conan")
+        
+    }
+    
+    @IBAction func pickImage(sender: AnyObject) {
+        
+        let  controller = UIImagePickerController()
+        controller.delegate = self
+        
+        switch sender.tag {
+        case SourceSelection.Album.rawValue:
+            controller.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+            print("Album picture selected.")
+        case SourceSelection.Camera.rawValue:
+            controller.sourceType = UIImagePickerControllerSourceType.Camera
+            print("Camera picture selected.")
+        default:
+            print("Error in pickImageFunction.")
+        }
+        presentViewController(controller, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        print("imagePickerController called.")
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imagePickedView.image = image
+        }
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        print("imagePickerControllerDidCancel called.")
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    /*func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+        textField.placeholder = nil
+        return true
+    }*/
+    func textFieldDidBeginEditing(textField: UITextField) {
+        if textField.text! == "TOP" || textField.text! == "BOTTOM" {
+            textField.text = ""
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+    
+    
+}
